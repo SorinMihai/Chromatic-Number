01998a6a8e6fe947045142d1dc9adea6f34c1b61
SorinMihai <33117840+SorinMihai@users.noreply.github.com>
2018-06-03 16:12:23 +0300

    Update chromatic_number.c

--- chromatic_number.c
+++ chromatic_number.c
@@ -12,7 +12,7 @@ int  chromatic_number(int **adjacency_matrix, int iterator)
 
     color_vector[iterator] = 1;
     for(row = 0; row < iterator; row++)
-    {
+    {   //if two neighbour vertices have the same color, assign a higher color
         if(adjacency_matrix[row][iterator] == 1 && color_vector[iterator] == color_vector[row])
         {
             color_vector[iterator] = color_vector[row]+1;
